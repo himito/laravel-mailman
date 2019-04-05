6260585f595f7af145de4b534c39db9155c43b08
Jaime Arias Almeida <arias@lipn.univ-paris13.fr>
2019-04-05 16:46:17 +0200

    Add empty list test

--- tests/MailmanFunctionTest.php
+++ tests/MailmanFunctionTest.php
@@ -4,21 +4,27 @@ namespace himito\mailman\Tests;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\HandlerStack;
 use himito\mailman\Mailman;
 
 class MailmanFunctionTest extends TestCase
 {
-    public function testLists()
+    /** @test */
+    public function it_returns_an_empty_list_when_there_are_no_mailing_lists()
     {
-        $mailman = $this->getMailman(200);
-        dd($mailman->lists());
+        $body = file_get_contents(__DIR__.'/Mocks/Lists/empty-body.txt');
+        $mailman = $this->get_mailman(200, $body);
+
+        $lists = $mailman->lists();
+        $this->assertEmpty($lists);
     }
 
-    private function getMailman($status, $body=null)
+    private function get_mailman($status, $body=null)
     {
         $mock = new MockHandler([new Response($status, [], $body)]);
         $handler = HandlerStack::create($mock);
+
         $client = new Client([
             'handler'=>$handler, 'base_uri' => 'http://mock.mailman.org']);
 
